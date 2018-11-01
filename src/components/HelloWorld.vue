b934381df5be0d66f478df8b08a859f6b6036891
yang1997 <424363283@qq.com>
2018-11-01 10:24:21 +0800

    d3关系图

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,49 @@
+<template>
+	<div class="hello" id="body">
+	</div>
+</template>
+
+<script>
+	import D3Relation from '@/components/d3-relation'
+
+	export default {
+		name: 'HelloWorld',
+		data() {
+			return {}
+		},
+		mounted() {
+			this.InTerFace();
+		},
+		methods: {
+		InTerFace:function(){		
+				$.getJSON('static/data.json', function(data) {
+					// let data = data;
+					// console.log(data);
+					// return;
+					new D3Relation({
+						nodes: data.nodes,
+						links: data.links,
+					})
+				})
+			}
+		}
+	}
+</script>
+<style scoped>
+	.tooltip {
+		position: fixed;
+		display: block;
+		padding: .4rem;
+		border-radius: .25rem;
+		background-color: rgba(0, 0, 0, .8);
+		white-space: nowrap;
+		color: #fff;
+		font-size: .8rem;
+	}
+
+	.tooltip a {
+		color: #658ccd;
+		cursor: pointer;
+		-webkit-tap-highlight-color: transparent;
+	}
+</style>
